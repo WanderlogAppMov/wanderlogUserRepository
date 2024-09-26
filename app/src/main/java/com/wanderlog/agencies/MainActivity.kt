5d44a5d51a0fe87b007389c4990430a59fe24a42
Gabotw <u202018627@upc.edu.pe>
2024-09-25 22:08:06 -0500

    feat: added connection HotelsEditorActivity.

--- app/src/main/java/com/wanderlog/agencies/MainActivity.kt
+++ app/src/main/java/com/wanderlog/agencies/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.wanderlog.agencies
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val imageView12: ImageView = findViewById(R.id.imageView12)
+        imageView12.setOnClickListener {
+            val intent = Intent(this, HotelsEditorActivity::class.java)
+            startActivity(intent)
         }
     }
 }
