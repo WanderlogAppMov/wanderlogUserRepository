bc564fe6179b37e288564a6502c6a72d06b44a7b
Fernando Díaz <u202112722@upc.edu.pe>
2024-09-25 21:44:44 -0500

    Add Activities Login and Main

---
+++ app/src/main/java/com/wanderlog/agencies/LoginActivity.kt
@@ -0,0 +1,32 @@
+package com.wanderlog.agencies
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Referencia al botón Sign In
+        val signInButton = findViewById<Button>(R.id.button2)
+
+        // Listener para redirigir a la vista principal
+        signInButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
