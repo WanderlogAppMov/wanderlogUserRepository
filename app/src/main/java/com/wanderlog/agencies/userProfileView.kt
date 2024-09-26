9935e516ff24532b6317a8b740fcad0467b74fdb
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-09-25 22:22:50 -0500

    feat: added return connection from userProfile to MainActivity.

--- app/src/main/java/com/wanderlog/agencies/userProfileView.kt
+++ app/src/main/java/com/wanderlog/agencies/userProfileView.kt
@@ -1,6 +1,8 @@
 package com.wanderlog.agencies
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class userProfileView : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val ivGoBack = findViewById<ImageView>(R.id.imageView)
+
+        ivGoBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
