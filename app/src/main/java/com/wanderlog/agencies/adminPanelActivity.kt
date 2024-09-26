af7fb4fc25927b1c62fd31a7fbc07fe20b042155
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-09-25 22:30:52 -0500

    feat: added return connection from AdminPanel to ManagePackages.

--- app/src/main/java/com/wanderlog/agencies/adminPanelActivity.kt
+++ app/src/main/java/com/wanderlog/agencies/adminPanelActivity.kt
@@ -1,6 +1,8 @@
 package com.wanderlog.agencies
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class adminPanelActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val ivGoActivityManagePackages = findViewById<ImageView>(R.id.imageView15)
+        ivGoActivityManagePackages.setOnClickListener {
+            val intent = Intent(this, PackagesEditorActivity::class.java)
+            startActivity(intent)
         }
     }
 }
